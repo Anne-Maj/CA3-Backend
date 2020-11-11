4ee2afab6c736fba718a6b2c3bf6a65e6a42f3a4
Anne-Maj Larsen <annemajlarsen@gmail.com>
2020-11-11 12:39:25 +0100

    JokeFetcher mm.

--- src/main/java/rest/JokeResource.java
+++ src/main/java/rest/JokeResource.java
@@ -1,11 +1,16 @@
 package rest;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import dtos.ChuckDTO;
 import dtos.CombinedJokesDTO;
 import dtos.DadDTO;
 import jokeEntities.ChuckJoke;
 import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeoutException;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Produces;
@@ -14,15 +19,18 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
 import javax.ws.rs.core.MediaType;
+import jokefetcher.JokeFetcher;
 import utils.HttpUtils;
 
 @Path("jokes")
 public class JokeResource {
+    private static final Gson gson = new GsonBuilder().setPrettyPrinting().create();
+    private static final ExecutorService es = Executors.newCachedThreadPool();
 
     @Context
     private UriInfo context;
     
-    Gson gson = new Gson();
+    //Gson gson = new Gson();
 
    
     @GET
@@ -41,6 +49,15 @@ public class JokeResource {
         return combinedJSON;
         
        }
+    
+    @GET
+    @Path("parallel")
+    @Produces (MediaType.APPLICATION_JSON)
+    public String getJokesParallel() throws IOException, InterruptedException, ExecutionException, TimeoutException {
+       String result = JokeFetcher.responseParallel(es, gson);
+       return result;
+    }
+    
 
    
 }
